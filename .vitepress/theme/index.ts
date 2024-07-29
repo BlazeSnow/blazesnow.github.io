b308dd0a9cc5723b84f7c3bf2d615916629d8b5f
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-07-29 21:02:34 +0800

    update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
-import layout_404 from './layout_404.vue'
+import Layout from './Layout.vue'
 
 export default {
 	extends: DefaultTheme,
-	Layout: layout_404
+	Layout
 }
