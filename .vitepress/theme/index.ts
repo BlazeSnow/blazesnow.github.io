cfa4e65190ca242757be642e894cad467b6977a4
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-10-06 23:10:13 +0800

    1

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,5 @@
-import DefaultTheme from 'vitepress/theme-without-fonts'
+import DefaultTheme from 'vitepress/theme'
+import "@fontsource/monaspace-neon/500.css";
 import './fonts.css'
 import './custom.css'
 import layout_404 from './404.vue'
