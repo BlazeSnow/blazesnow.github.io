4a70927ecb803d7787300b6f232c5258b54f1284
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-10-06 23:26:17 +0800

    update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 import DefaultTheme from 'vitepress/theme'
-import "@fontsource/monaspace-neon/500.css";
+import "@fontsource/monaspace-neon";
 import './fonts.css'
 import './custom.css'
 import layout_404 from './404.vue'
