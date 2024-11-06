5066b08b347e836c4cd6f6acb84723fa729254b5
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-11-06 20:00:03 +0800

    1

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,9 +2,17 @@ import DefaultTheme from 'vitepress/theme'
 import "@fontsource/monaspace-neon";
 import './fonts.css'
 import './custom.css'
-import layout from './Layout.vue';
+import layoutwithads from './Layout.vue';
+import layout from './404.vue'
 
 export default {
 	extends: DefaultTheme,
-	Layout: layout,
+	enhanceApp({ app }) {
+		const hostname = window.location.hostname;
+		if (hostname === 'example.org' || hostname === 'www.example.org') {
+			app.component('Layout', layoutwithads);
+		} else {
+			app.component('Layout', layout);
+		}
+	}
 }
