8429d454dc78c7059dd98250bcb97f36798f2c8d
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-11-06 20:13:47 +0800

    1

--- .vitepress/theme/layout.vue
+++ .vitepress/theme/layout.vue
@@ -6,6 +6,14 @@ const { Layout } = DefaultTheme
 
 <template>
     <Layout>
+        <!-- ads -->
+        <template #aside-ads-after>
+            <amp-ad width="100vw" height="320" type="adsense" data-ad-client="ca-pub-8010307323433130"
+                data-ad-slot="3760107648" data-auto-format="rspv" data-full-width="">
+                <div overflow=""></div>
+            </amp-ad>
+        </template>
+        <!-- 404 -->
         <template #not-found>
             <div id="VPContent" class="VPContent">
                 <div class="NotFound">
