ad4bae6e4b42ee2442f7d4b3d91f408d34894804
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-11-06 19:31:41 +0800

    1

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,94 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+    <Layout>
+        <!-- ads -->
+        <template #aside-ads-after>
+            <amp-ad layout="fixed" width="224" height="224" type="adsense" data-ad-client="ca-pub-8010307323433130"
+                data-ad-slot="9161174417">
+            </amp-ad>
+        </template>
+        <!-- 404 -->
+        <template #not-found>
+            <div id="VPContent" class="VPContent">
+                <div class="NotFound">
+                    <p class="code">
+                        404
+                    </p>
+                    <h1 class="title">
+                        页面未找到
+                    </h1>
+                    <div class="divider"></div>
+                    <blockquote class="quote">
+                        抱歉，没有找到您需要的页面
+                    </blockquote>
+                    <div class="action">
+                        <a aria-label="回到主页" class="VPButton medium brand" href="/">
+                            回到主页
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </template>
+    </Layout>
+</template>
+
+<style scoped>
+div.NotFound {
+    padding: 64px 24px 96px;
+    text-align: center;
+}
+
+div.NotFound p.code {
+    line-height: 92px;
+    font-size: 92px;
+    font-weight: 600;
+}
+
+div.NotFound h1.title {
+    padding-top: 12px;
+    letter-spacing: 2px;
+    line-height: 40px;
+    font-size: 40px;
+    font-weight: 700;
+}
+
+div.NotFound div.divider {
+    margin: 24px auto 18px;
+    width: 300px;
+    height: 1px;
+    background-color: var(--vp-c-divider);
+}
+
+div.NotFound blockquote.quote {
+    margin: 0 auto;
+    max-width: fit-content;
+    line-height: 1.6;
+    font-size: 20px;
+    font-weight: 500;
+    color: var(--vp-c-text-2);
+}
+
+div.NotFound div.action {
+    padding-top: 50px;
+}
+
+div.NotFound a.VPButton.medium.brand {
+    display: inline-block;
+    border: 1px solid transparent;
+    text-align: center;
+    font-weight: 600;
+    white-space: nowrap;
+    transition: color .25s, border-color .25s, background-color .25s;
+    border-radius: 20px;
+    padding: 0 20px;
+    line-height: 38px;
+    font-size: 14px;
+    color: var(--vp-button-brand-text);
+    background-color: var(--vp-button-brand-bg);
+}
+</style>
