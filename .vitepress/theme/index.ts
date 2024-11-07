398b2e71e3dc25139b6b9d3e977326e90885cfdb
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-11-07 11:34:47 +0800

    googleAnalytics

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,5 @@
 import DefaultTheme from 'vitepress/theme'
+import googleAnalytics from 'vitepress-plugin-google-analytics'
 import "@fontsource/monaspace-neon";
 import './fonts.css'
 import './custom.css'
@@ -7,4 +8,9 @@ import template from './template.vue';
 export default {
 	extends: DefaultTheme,
 	Layout: template,
+	enhanceApp: (ctx) => {
+		googleAnalytics({
+			id: 'AW-16752995804',
+		})
+	}
 }
