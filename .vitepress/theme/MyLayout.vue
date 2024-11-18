d0e56566f7164c0d61ea29c8db8257c6d8d26138
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-11-18 14:04:04 +0800

    MyLayout

---
+++ .vitepress/theme/MyLayout.vue
@@ -0,0 +1,8 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import layout_404 from './404.vue'
+</script>
+
+<template>
+    <layout_404 />
+</template>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,16 +1,10 @@
-import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import "@fontsource/monaspace-neon";
 import './fonts.css'
 import './custom.css'
-import layout_404 from './404.vue';
+import MyLayout from './MyLayout.vue';
 
 export default {
 	extends: DefaultTheme,
-	Layout: () => {
-		return h(DefaultTheme.Layout, null, {
-			// 404
-			'not-found': () => h(layout_404),
-		})
-	},
+	Layout: MyLayout
 }
