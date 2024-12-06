87243bc23f94c45d0df957350c9d08402712f79a
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-12-06 21:54:27 +0800

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,9 +2,9 @@ import DefaultTheme from 'vitepress/theme'
 import "@fontsource/monaspace-neon/400.css";
 import './fonts.css'
 import './custom.css'
-import MyLayout from './MyLayout.vue';
+// import MyLayout from './MyLayout.vue';
 
 export default {
 	extends: DefaultTheme,
-	Layout: MyLayout
+	// Layout: MyLayout,
 }
