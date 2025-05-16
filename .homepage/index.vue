5d8836ce22631158c525cfb34d99eaa224dfd733
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-05-16 12:15:34 +0800

    Dev (#349)
    * update
    
    * Update index.md

--- .homepage/index.vue
+++ .homepage/index.vue
@@ -4,7 +4,7 @@ import card from "./card.vue"
 
 <template>
   <h2>
-    系统：操作系统相关
+    系统：解决操作系统问题
   </h2>
   <card title="Windows" link="/Windows/">
     <template #description>
@@ -26,45 +26,12 @@ import card from "./card.vue"
   </card>
   <card title="搭建内网穿透服务" link="/FrpClientAndServer/">
     <template #description>
-      使用fatedier/frp在Windows客户端和Ubuntu服务端上搭建frp内网穿透服务
-    </template>
-  </card>
-  <card title="Nginx" link="/Nginx/">
-    <template #description>
-      Nginx是一款HTTP Web服务器
-    </template>
-  </card>
-  <h2>
-    更多内容
-  </h2>
-  <card title="DNS加密" link="/DNSEncryption/">
-    <template #description>
-      加密DNS通过对查询和响应进行加密，确保数据在传输过程中不被窃听或篡改
-    </template>
-  </card>
-  <card title="Android" link="/Android/">
-    <template #description>
-      Android是一个基于Linux核心与其他开源软件的开放源代码的移动操作系统
-    </template>
-  </card>
-  <card title="Nodejs" link="/Nodejs/">
-    <template #description>
-      Node.js是跨平台、开源的JavaScript运行环境
-    </template>
-  </card>
-  <card title="Docker" link="/Docker/">
-    <template #description>
-      Docker是一个开放源代码的开放平台软件，用于开发、交付和运行应用
-    </template>
-  </card>
-  <card title="Clang" link="/Clang/">
-    <template #description>
-      Clang是一个C、C++、Objective-C和Objective-C++编程语言的编译器前端
+      在Windows客户端和Ubuntu服务端上搭建内网穿透服务
     </template>
   </card>
   <card title="VitePress建站教程" link="/VitePress/">
     <template #description>
-      通过VitePress和GitHub Pages生成静态站点的教程
+      通过VitePress和GitHub Pages生成静态站点
     </template>
   </card>
   <card title="部署网站至服务器" link="/WebsiteToServer/">
@@ -73,16 +40,9 @@ import card from "./card.vue"
     </template>
   </card>
   <h2>
-    关于
+    更多内容
   </h2>
-  <card title="更新日志" link="/CHANGELOG/">
-    <template #description>
-      本网站的更新日志
-    </template>
-  </card>
-  <card title="关于本站" link="/about">
-    <template #description>
-      本网站的更多信息
-    </template>
-  </card>
+  <h4>
+    更多内容详见网页顶栏
+  </h4>
 </template>
