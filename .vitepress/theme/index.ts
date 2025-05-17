03dc4cb3ca4176f74b7f7c8ee4ef819f8560453e
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-05-17 11:32:04 +0800

    Dev (#350)
    * update
    
    * Update index.ts
    
    * Update index.md

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,12 +3,18 @@ import './fonts.css'
 import './color.css'
 import { h } from 'vue'
 import layout_404 from './404.vue'
+import googleAnalytics from 'vitepress-plugin-google-analytics'
 
 export default {
 	extends: DefaultTheme,
 	Layout() {
 		return h(DefaultTheme.Layout, null, {
 			'not-found': () => h(layout_404)
+		})
+	},
+	enhanceApp: (ctx) => {
+		googleAnalytics({
+			id: 'GT-KVFR6NJP',
 		})
 	},
 }
