290cef34e8e152f2640a0ef7bc2885d60e21797d
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-05-17 11:24:05 +0800

    update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,12 +3,18 @@ import './fonts.css'
 import './color.css'
 import { h } from 'vue'
 import layout_404 from './404.vue'
+import googleAnalytics from 'vitepress-plugin-google-analytics'
 
 export default {
 	extends: DefaultTheme,
 	Layout() {
 		return h(DefaultTheme.Layout, null, {
 			'not-found': () => h(layout_404)
+		})
+	},
+	enhanceApp: (ctx) => {
+		googleAnalytics({
+			id: 'G-N6Q98Q8D1N',
 		})
 	},
 }
