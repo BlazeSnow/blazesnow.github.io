bcc95bfdfa2f6341d1b0478714f5de42db1723a5
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-08-03 19:04:46 +0800

    Update card.vue

--- Homepage/card.vue
+++ Homepage/card.vue
@@ -65,6 +65,7 @@ export default {
     display: -webkit-box;
     -webkit-box-orient: vertical;
     -webkit-line-clamp: 3;
+    line-clamp: 3;
     overflow: hidden;
     text-overflow: ellipsis;
 }
