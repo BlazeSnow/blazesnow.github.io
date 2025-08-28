ed941139d62ec0672490f69ff4d855df69b7c50b
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-08-28 21:37:40 +0800

    Dev (#394)
    * update
    
    * update
    
    * update
    
    * Update package-lock.json
    
    * Update status.vue
    
    * Update index.md

---
+++ .homepage/status.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    if (typeof window !== 'undefined') {
+        import('@webcomponents/webcomponentsjs/webcomponents-bundle.js')
+        import('@statuspage/status-widget/dist/index.js')
+    }
+})
+</script>
+
+<template>
+    <h2>网站状态</h2>
+    <statuspage-widget src="https://blazesnow.statuspage.io" appearance="basic" title="BlazeSnow网站状态">
+    </statuspage-widget>
+    <p>
+        查看本网站的运行状态及可用域名：
+        <a href="https://blazesnow.statuspage.io/" target="_blank">
+            https://blazesnow.statuspage.io/
+        </a>
+    </p>
+</template>
