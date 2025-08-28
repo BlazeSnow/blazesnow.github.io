7d1f53294ef27e5455d0876c6afe6aba58b80c6a
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-08-28 21:33:32 +0800

    Update status.vue

--- .homepage/status.vue
+++ .homepage/status.vue
@@ -13,4 +13,10 @@ onMounted(() => {
     <h2>网站状态</h2>
     <statuspage-widget src="https://blazesnow.statuspage.io" appearance="basic" title="BlazeSnow网站状态">
     </statuspage-widget>
+    <p>
+        查看本网站的运行状态及可用域名：
+        <a href="https://blazesnow.statuspage.io/" target="_blank">
+            https://blazesnow.statuspage.io/
+        </a>
+    </p>
 </template>
