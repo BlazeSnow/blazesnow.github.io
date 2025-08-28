efd3f5035829deea0231b7b7ebc3e6391c5b4d8b
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-08-28 21:29:57 +0800

    update

--- .homepage/status.vue
+++ .homepage/status.vue
@@ -1,5 +1,16 @@
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    if (typeof window !== 'undefined') {
+        import('@webcomponents/webcomponentsjs/webcomponents-bundle.js')
+        import('@statuspage/status-widget/dist/index.js')
+    }
+})
+</script>
+
 <template>
     <h2>网站状态</h2>
-    <statuspage-widget src="https://blazesnow.statuspage.io" appearance="basic"
-        title="BlazeSnow网站状态"></statuspage-widget>
+    <statuspage-widget src="https://blazesnow.statuspage.io" appearance="basic" title="BlazeSnow网站状态">
+    </statuspage-widget>
 </template>
