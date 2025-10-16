00be4f80a7a5f8c9fa2c1ae53398dee2b83ed191
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-10-16 13:42:46 +0800

    Add IPAbuyer documentation and assets
    Introduces a new 'IPAbuyer' section to the site navigation, adds documentation for IPAbuyer in ipa/index.md, includes related images, and provides a Vue component for the Microsoft Store badge.

---
+++ ipa/microsoft.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    if (typeof window !== 'undefined') {
+        const script = document.createElement('script')
+        script.type = 'module'
+        script.src = 'https://get.microsoft.com/badge/ms-store-badge.bundled.js'
+        document.head.appendChild(script)
+    }
+})
+</script>
+
+<template>
+    <ms-store-badge productid="9msxmxd5520x" window-mode="full" theme="auto" size="large" language="zh-cn"
+        animation="on">
+    </ms-store-badge>
+</template>
