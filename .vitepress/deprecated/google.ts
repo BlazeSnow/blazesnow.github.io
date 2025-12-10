1c1ae895c12300b0e023c5d13e67fa535207bada
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-12-10 17:41:50 +0800

    Delete google.ts

--- .vitepress/deprecated/google.ts
+++
@@ -1,9 +0,0 @@
-import googleAnalytics from 'vitepress-plugin-google-analytics'
-
-export default {
-    enhanceApp: (ctx) => {
-        googleAnalytics({
-            id: 'G-1KLV0EED8K',
-        })
-    }
-}
