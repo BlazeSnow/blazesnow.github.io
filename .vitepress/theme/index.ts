0f6492cca4abe2de7d0caf87928aa63bd1147257
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-12-10 17:42:52 +0800

    Remove ImagePorter and Google Analytics integration (#409)
    * Remove ImagePorter and Google Analytics integration
    
    Deleted ImagePorter documentation and related navigation links. Removed Google Analytics integration from the theme and dependencies. Added redirect for /imageporter to the GitHub repository. Updated changelog and deprecated/redirect documentation accordingly.
    
    * Delete google.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,19 +3,13 @@ import './fonts.css'
 import './color.css'
 import { h } from 'vue'
 import layout_404 from './404.vue'
-import '@fontsource/monaspace-neon/400.css';
-import googleAnalytics from 'vitepress-plugin-google-analytics'
+import '@fontsource/monaspace-neon';
 
 export default {
 	extends: DefaultTheme,
 	Layout() {
 		return h(DefaultTheme.Layout, null, {
 			'not-found': () => h(layout_404)
-		})
-	},
-	enhanceApp: (ctx) => {
-		googleAnalytics({
-			id: 'G-1KLV0EED8K',
 		})
 	}
 }
