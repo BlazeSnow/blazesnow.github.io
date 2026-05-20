32fdfd27ec61e0d68dee334480c66e4cd36485c0
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2026-05-20 16:20:47 +0800

    windows.svg

---
+++ .vitepress/theme/TitleIcon.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+	src: {
+		type: String,
+		required: true
+	},
+	alt: {
+		type: String,
+		required: false,
+		default: ''
+	}
+})
+</script>
+
+<template>
+	<img class="title-icon" :src="src" :alt="alt" :aria-hidden="alt ? null : 'true'">
+</template>
+
+<style scoped>
+.title-icon {
+	display: inline-block;
+	width: 1em;
+	height: 1em;
+	margin-right: 0.35em;
+	vertical-align: -0.12em;
+}
+</style>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,12 +4,14 @@ import './color.css'
 import { h } from 'vue'
 import layout_404 from './404.vue'
 import FirstPartyBadge from './FirstPartyBadge.vue'
+import TitleIcon from './TitleIcon.vue'
 import '@fontsource/monaspace-neon';
 
 export default {
 	extends: DefaultTheme,
 	enhanceApp({ app }) {
 		app.component('FirstPartyBadge', FirstPartyBadge)
+		app.component('TitleIcon', TitleIcon)
 	},
 	Layout() {
 		return h(DefaultTheme.Layout, null, {
