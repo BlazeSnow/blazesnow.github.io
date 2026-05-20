c3c145c3c0ba43920cabf2cdfd3a83b10943ee4b
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2026-05-20 18:07:14 +0800

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,6 +4,7 @@ import './color.css'
 import { h } from 'vue'
 import layout_404 from './404.vue'
 import FirstPartyBadge from './FirstPartyBadge.vue'
+import StopSupportBadge from './StopSupportBadge.vue'
 import TitleIcon from './TitleIcon.vue'
 import '@fontsource/monaspace-neon';
 
@@ -11,6 +12,7 @@ export default {
 	extends: DefaultTheme,
 	enhanceApp({ app }) {
 		app.component('FirstPartyBadge', FirstPartyBadge)
+		app.component('StopSupportBadge', StopSupportBadge)
 		app.component('TitleIcon', TitleIcon)
 	},
 	Layout() {
