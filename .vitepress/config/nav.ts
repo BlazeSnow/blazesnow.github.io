a1fb04e6cba2cf766d83bfb367929ae0b9091624
BlazeSnow <git@blazesnow.org>
2026-09-21 18:43:04 +0800

    Update nav.ts

--- .vitepress/config/nav.ts
+++ .vitepress/config/nav.ts
@@ -15,8 +15,8 @@ export const nav: DefaultTheme.NavItem[] = [
       },
       {
         text: 'PowerPlan',
-        link: '/powerplan/',
-        activeMatch: '/powerplan/'
+        link: 'https://powerplan.blazesnow.com/',
+        target: '_blank'
       },
       {
         text: 'Todo4Agent',
